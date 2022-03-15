92b617757e26fe72527aab1bc28a0aa8e55b7d5e
rossrobino <rbrobino@gmail.com>
2022-03-15 16:27:46 -0700

    clean up, add header animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,3 +4,15 @@
     linked to index.html
 */
 
+/* header animation */
+window.addEventListener("scroll", function() {
+    const distance = window.scrollY;
+    document.querySelector("header").style.transform = `translateY(${distance * 0.5}px)`;
+    if (distance < 720) {
+        document.querySelector("h1").style.fontSize = `${distance * window.innerWidth/8000}px`;
+        document.querySelector("h1").style.transform = `rotate(${distance * 0.5}deg)`;
+    } else {
+        document.querySelector("h1").style.fontSize = `${0.09 * window.innerWidth}px`;
+        document.querySelector("h1").style.transform = 'rotate(0deg)';
+    }
+  });
