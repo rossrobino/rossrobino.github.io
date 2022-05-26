09c25acf2180b96cdbb6b9032b48ab792cf60aa1
rossrobino <rbrobino@gmail.com>
2022-05-26 15:21:05 -0700

    update header

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,9 +8,4 @@
 window.addEventListener("scroll", function() {
     const distance = window.scrollY;
     document.querySelector("header").style.transform = `translateY(${distance * 0.5}px)`;
-    if (distance < 250) {
-        document.querySelector("h1").style.fontSize = `${32 + (distance * window.innerWidth/3000)}px`;
-    } else {
-        document.querySelector("h1").style.fontSize = `${32 + (250 * window.innerWidth/3000)}px`;
-    }
 });
